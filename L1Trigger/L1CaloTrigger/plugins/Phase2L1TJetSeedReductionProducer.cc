12a0160fbceb9a475a2f680ea53d1ac9f593eb8e
Liam <liam.robertshaw@cern.ch>
2025-02-18 11:48:40 +0000

    fixed bugs so EDProducer now compiles

--- L1Trigger/L1CaloTrigger/plugins/Phase2L1TJetSeedReductionProducer.cc
+++ L1Trigger/L1CaloTrigger/plugins/Phase2L1TJetSeedReductionProducer.cc
@@ -14,6 +14,9 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/StreamID.h"
 
+#include "DataFormats/L1TParticleFlow/interface/PFCandidate.h"
+
+
 class Phase2L1TJetSeedReductionProducer : public edm::stream::EDProducer<> {
 public:
   explicit Phase2L1TJetSeedReductionProducer(const edm::ParameterSet&);
@@ -36,11 +39,14 @@ private:
   // ----------member data ---------------------------
   edm::EDGetTokenT<std::vector<l1t::PFCandidate>> seedsToken;
   std::string outputCollectionName_;
+
+  l1t::PFCandidateCollection copyInputToOutput(l1t::PFCandidateCollection seeds);
+
 };
 
 Phase2L1TJetSeedReductionProducer::Phase2L1TJetSeedReductionProducer(const edm::ParameterSet& cfg) 
   : seedsToken(consumes<l1t::PFCandidateCollection>(cfg.getParameter<edm::InputTag>("histoSeeds"))),
-    outputCollectionName_(iConfig.getParameter<std::string>("outputCollectionName")) 
+    outputCollectionName_(cfg.getParameter<std::string>("outputCollectionName")) 
 {
   //register your products
   /* Examples
@@ -56,28 +62,35 @@ Phase2L1TJetSeedReductionProducer::Phase2L1TJetSeedReductionProducer(const edm::
   produces<l1t::PFCandidateCollection>(outputCollectionName_);
 }
 
-void Phase2L1TJetSeedReductionProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) const {
+void Phase2L1TJetSeedReductionProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
   // GET SEEDS FROM EDM INPUT
   edm::Handle<l1t::PFCandidateCollection> seedsHandle;
-  std::vector<edm::Ptr<l1t::PFCandidate>> seeds;
+  // std::vector<edm::Ptr<l1t::PFCandidate>> seeds;
+  l1t::PFCandidateCollection seeds;
   iEvent.getByToken(seedsToken, seedsHandle);
   for (unsigned i = 0; i < (*seedsHandle).size(); i++) {
-    seeds.push_back(edm::Ptr<l1t::PFCandidate>(seedsHandle, i));
+    edm::Ptr<l1t::PFCandidate> seedPtr(seedsHandle, i);    // get pointer to seed i
+    seeds.push_back(*seedPtr);    // dereference seed i pointer to get seed i and add to seeds vector
+  }
+  // WE NOW HAVE A VECTOR OF THE SEEDS PASSED TO THE MODULE
+
+  // NOW WE WANT TO WRITE THE LOGIC WHICH WILL REDUCE THE SEEDS TO PRODUCE THE WIDE JET SEEDS
+  // AVOID WRITING ALL THE CODE IN THE PRODUCE FUNCTION, INSTEAD BREAK INTO SMALLER MORE MODULAR FUNCTIONS (I.E. DELTA R MATCHING, ETC)
+  // AS AN EXAMPLE, I HAVE WRITTEN A FUNCTION WHICH COPIES THE VECTOR OF SEEDS TO A NEW VECTOR OF SEEDS:
+  l1t::PFCandidateCollection reducedSeeds = copyInputToOutput(seeds);
+
+  int i = 0;
+  for(auto& seed : reducedSeeds) {
+    i += 1;
+    std::cout << "Seed "<< i << ": " << seed.pt() << std::endl;
+    // std::cout << "Seed "<< i << ": " << seed.pt() << std::endl;
   }
 
-  /*
-  YOUR CODE HERE, EXAMPLE:
+  std::unique_ptr<l1t::PFCandidateCollection> reducedSeedsPtr;//(new l1t::PFCandidateCollection);
+  reducedSeedsPtr->swap(reducedSeeds);
 
-  l1t::PFCandidateCollection reduceSeeds(std::vector<edm::Ptr<l1t::PFCandidate>> seeds){
-    REDUCE HSC4 SEEDS TO HSC8 SEEDS LOGIC HERE
-  }
-
-  */
-
-  // prepare output and put into the event
-  l1t::PFCandidateCollection reducedSeeds;
-  iEvent.put(std::move(reducedSeeds), outputCollectionName_);
+  iEvent.put(std::move(reducedSeedsPtr), outputCollectionName_);    // NOTE CAN ONLY PUT POINTERS INTO THE EVENT, NOT THE OBJECTS THEMSELVES
 }
 
 /* DESTRUCTOR */
@@ -87,6 +100,13 @@ Phase2L1TJetSeedReductionProducer::~Phase2L1TJetSeedReductionProducer() {
   //
   // please remove this method altogether if it would be left empty
 }
+
+l1t::PFCandidateCollection Phase2L1TJetSeedReductionProducer::copyInputToOutput(l1t::PFCandidateCollection seeds){
+  l1t::PFCandidateCollection reducedSeeds;
+  std::copy(seeds.begin(), seeds.end(), std::back_inserter(reducedSeeds));
+  return reducedSeeds;
+}
+
 
 /*
 // ------------ method called once each stream before processing any runs, lumis or events  ------------
@@ -99,39 +119,23 @@ void Phase2L1TJetSeedReductionProducer::endStream() {
   // please remove this method if not needed
 }
 
-/*
 // ------------ method called when starting to processes a run  ------------
-/*
-void
-Phase2L1TJetSeedReductionProducer::beginRun(edm::Run const&, edm::EventSetup const&)
-{
+void Phase2L1TJetSeedReductionProducer::beginRun(edm::Run const&, edm::EventSetup const&) {
 }
-*/
 
 // ------------ method called when ending the processing of a run  ------------
-/*
-void
-Phase2L1TJetSeedReductionProducer::endRun(edm::Run const&, edm::EventSetup const&)
-{
+void Phase2L1TJetSeedReductionProducer::endRun(edm::Run const&, edm::EventSetup const&) {
 }
-*/
 
 // ------------ method called when starting to processes a luminosity block  ------------
-/*
-void
-Phase2L1TJetSeedReductionProducer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
-{
+void Phase2L1TJetSeedReductionProducer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) {
 }
-*/
 
 // ------------ method called when ending the processing of a luminosity block  ------------
-/*
-void
-Phase2L1TJetSeedReductionProducer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
-{
+
+void Phase2L1TJetSeedReductionProducer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) {
 }
 */
-
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void Phase2L1TJetSeedReductionProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
@@ -139,9 +143,9 @@ void Phase2L1TJetSeedReductionProducer::fillDescriptions(edm::ConfigurationDescr
   // Please change this to state exactly what you do use, even if it is no parameters
   edm::ParameterSetDescription desc;
 
-  desc.add<edm::InputTag>("seeds", edm::InputTag('l1tHSC8PFL1PuppiSimSeedProducer', 'WIDEHSCEMUSEEDS'));
+  desc.add<edm::InputTag>("seeds", edm::InputTag("l1tHSC8PFL1PuppiSimSeedProducer", "WIDEHSCEMUSEEDS"));
   
-  descriptions.add(desc);
+  descriptions.addWithDefaultLabel(desc);
 }
 
 //define this as a plug-in
