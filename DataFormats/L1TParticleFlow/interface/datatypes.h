2bf34709337262716706cd2a62d83fa4b2e707d4
Liam <liam.robertshaw@cern.ch>
2025-02-20 15:58:46 +0000

    modified datatypes and methods to handle jet mass

--- DataFormats/L1TParticleFlow/interface/datatypes.h
+++ DataFormats/L1TParticleFlow/interface/datatypes.h
@@ -8,6 +8,7 @@
 namespace l1ct {
 
   typedef ap_ufixed<14, 12, AP_TRN, AP_SAT> pt_t;
+  typedef ap_ufixed<10, 8, AP_TRN, AP_SAT> mass_t; //typedef ap_ufixed<12, 10, AP_TRN, AP_SAT> mass_t; using this precision causes an error
   typedef ap_ufixed<10, 8, AP_TRN, AP_SAT> pt10_t;
   typedef ap_fixed<16, 14, AP_TRN, AP_SAT> dpt_t;
   typedef ap_ufixed<28, 24, AP_TRN, AP_SAT> pt2_t;
@@ -176,7 +177,8 @@ namespace l1ct {
     inline float floatMeanZ(meanz_t meanz) { return meanz + MEANZ_OFFSET; };
     inline float floatHoe(hoe_t hoe) { return hoe.to_float(); };
     inline float floatIDScore(id_score_t score) { return score.to_float(); };
-    inline float floatBtagScore(b_tag_score_t b_tag_score) { return b_tag_score.to_float(); }
+    // inline float floatBtagScore(b_tag_score_t b_tag_score) { return b_tag_score.to_float(); }
+    inline float floatMass(mass_t mass) { return mass.to_float(); }
 
     inline pt_t makePt(int pt) { return ap_ufixed<16, 14>(pt) >> 2; }
     inline dpt_t makeDPt(int dpt) { return ap_fixed<18, 16>(dpt) >> 2; }
