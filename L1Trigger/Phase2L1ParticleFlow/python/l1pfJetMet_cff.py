c8b64d6f48fcf844b6975d3f94a358247485475f
Liam <liam.robertshaw@cern.ch>
2025-03-14 12:19:15 +0000

    added sc8 corrected just to python config

--- L1Trigger/Phase2L1ParticleFlow/python/l1pfJetMet_cff.py
+++ L1Trigger/Phase2L1ParticleFlow/python/l1pfJetMet_cff.py
@@ -12,7 +12,14 @@ l1tSC8PFL1PF            = l1SeedConePFJetProducer.clone(L1PFObjects = 'l1tLayer1
 l1tSC8PFL1Puppi         = l1SeedConePFJetProducer.clone(coneSize = cms.double(0.8))
 l1tSC8PFL1PuppiEmulator = l1SeedConePFJetEmulatorProducer.clone(L1PFObjects = 'l1tLayer2Deregionizer:Puppi',
                                                                  coneSize = cms.double(0.8))
-
+l1tSC4PFL1PuppiCorrected         = l1SeedConePFJetProducer.clone(coneSize = cms.double(0.4),
+                                                        doCorrections = cms.bool(True),
+                                                        correctorFile = cms.string("L1Trigger/Phase2L1ParticleFlow/data/jecs/jecs_20220308.root"),
+                                                        correctorDir = cms.string('L1PuppiSC4EmuJets'))
+l1tSC8PFL1PuppiCorrected         = l1SeedConePFJetProducer.clone(coneSize = cms.double(0.8),
+                                                        doCorrections = cms.bool(True),
+                                                        correctorFile = cms.string("L1Trigger/Phase2L1ParticleFlow/data/jecs/jecs_20220308.root"),
+                                                        correctorDir = cms.string('L1PuppiSC4EmuJets'))
 l1tSC4PFL1PuppiCorrectedEmulator = l1SeedConePFJetEmulatorProducer.clone(L1PFObjects = 'l1tLayer2Deregionizer:Puppi',
                                                                           doCorrections = cms.bool(True),
                                                                           correctorFile = cms.string("L1Trigger/Phase2L1ParticleFlow/data/jecs/jecs_20220308.root"),
@@ -48,7 +55,7 @@ l1tSC4PFL1PuppiExtendedCorrectedEmulator = l1SeedConePFJetEmulatorProducer.clone
                                                                      correctorDir = cms.string('L1PuppiSC4EmuJets'))
 
 L1TPFJetsTask = cms.Task(
-    l1tLayer2Deregionizer, l1tSC4PFL1PF, l1tSC4PFL1Puppi, l1tSC8PFL1Puppi, l1tSC4PFL1PuppiEmulator, l1tSC4PFL1PuppiCorrectedEmulator, l1tSC4PFL1PuppiCorrectedEmulatorMHT,
+    l1tLayer2Deregionizer, l1tSC4PFL1PF, l1tSC4PFL1Puppi, l1tSC4PFL1PuppiCorrected, l1tSC8PFL1Puppi, l1tSC8PFL1PuppiCorrected, l1tSC4PFL1PuppiEmulator, l1tSC4PFL1PuppiCorrectedEmulator, l1tSC4PFL1PuppiCorrectedEmulatorMHT,
     l1tSC8PFL1PuppiEmulator, l1tSC8PFL1PuppiCorrectedEmulator
 )
 
