64e58a28bc87a0993fe7558b258876c023688340
Liam Robertshaw <liam.robertshaw@cern.ch>
2025-05-06 15:43:30 +0200

    further merge conflict resolutions

--- DataFormats/L1TParticleFlow/interface/gt_datatypes.h
+++ DataFormats/L1TParticleFlow/interface/gt_datatypes.h
@@ -27,6 +27,7 @@ namespace l1gt {
   // While bitwise identical to the l1ct::z0_t value, we store z0 in mm to profit of ap_fixed goodies
   typedef ap_fixed<10, 9, AP_RND_CONV, AP_SAT> z0_t;   // NOTE: mm instead of cm!!!
   typedef ap_ufixed<8, 1, AP_RND, AP_SAT> id_proba_t;  // for IDs bounded in range [0-1]
+  typedef ap_ufixed<10, 1, AP_RND, AP_SAT> n_prong_score_t;
   typedef ap_ufixed<17, 15, AP_RND_CONV, AP_SAT> mass2_t;
   typedef ap_uint<1> valid_t;
 
